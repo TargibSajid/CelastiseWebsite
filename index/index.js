17320503a04b06e049803d8df93ce19a0f691e07
Targib Sajid <167701006+TargibSajid@users.noreply.github.com>
2025-11-22 14:04:50 +0600

    Was not neccessary

--- index/index.js
+++
@@ -1,7 +0,0 @@
-let count = document.getElementById('count').innerText;
-count = parseInt(count);
-
-function increase() {
-    count++; // increase the number
-    document.getElementById('count').innerText = count; // update the HTML
-}
